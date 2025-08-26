5e5bee3fd37ca5d0e8cecb3684041d7c653fd8d4
kateyurasova <katsiaryna.yurasava@gmail.com>
2025-08-26 20:28:49 +0200

    Added WebdriverManager to run tests in Chrome and Firefox

--- src/test/java/com/example/LoginTest.java
+++ src/test/java/com/example/LoginTest.java
@@ -1,87 +1,70 @@
 package com.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.*;
-
-import java.time.Duration;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 public class LoginTest {
-
-    private WebDriver driver;
+    WebDriver driver;
 
     @BeforeMethod
-    public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
+    @Parameters("browser")
+    public void setUp(String browser) {
+        switch (browser.toLowerCase()) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+        }
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/login");
+    }
+
+    @Test
+    public void testSuccessfulLogin() {
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.cssSelector("button.radius"));
+
+        username.sendKeys("tomsmith");
+        password.sendKeys("SuperSecretPassword!");
+        loginButton.click();
+
+        WebElement successMsg = driver.findElement(By.id("flash"));
+        Assert.assertTrue(successMsg.getText().contains("You logged into a secure area!"),
+                "Success message is not displayed!");
+    }
+
+    @Test
+    public void testInvalidPassword() {
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.cssSelector("button.radius"));
+
+        username.sendKeys("tomsmith");
+        password.sendKeys("wrongPassword");
+        loginButton.click();
+
+        WebElement errorMsg = driver.findElement(By.id("flash"));
+        Assert.assertTrue(errorMsg.getText().contains("Your password is invalid!"),
+                "Error message is not displayed!");
     }
 
     @AfterMethod
     public void tearDown() {
-        if(driver != null) {
+        if (driver != null) {
             driver.quit();
         }
-    }
-
-    // ----------------- DataProvider для позитивных тестов -----------------
-    @DataProvider(name = "validCredentials")
-    public Object[][] validCredentials() {
-        return new Object[][] {
-                {"tomsmith", "SuperSecretPassword!"}
-                // It is possible to add some more users to check boundary values
-        };
-    }
-
-    @Test(dataProvider = "validCredentials")
-    public void testSuccessfulLogin(String usernameValue, String passwordValue) {
-        driver.get("https://the-internet.herokuapp.com/login");
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        WebElement usernameInput = wait.until(ExpectedConditions.elementToBeClickable(By.id("username")));
-        WebElement passwordInput = driver.findElement(By.id("password"));
-        WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
-
-        usernameInput.sendKeys(usernameValue);
-        passwordInput.sendKeys(passwordValue);
-        loginButton.click();
-
-        WebElement successMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("flash")));
-        Assert.assertTrue(successMessage.getText().contains("You logged into a secure area!"));
-    }
-
-    // ----------------- DataProvider для негативных тестов -----------------
-    @DataProvider(name = "invalidCredentials")
-    public Object[][] invalidCredentials() {
-        return new Object[][] {
-                {"wronguser", "wrongpassword", "Your username is invalid!"},
-                {"tomsmith", "wrongpassword", "Your password is invalid!"},
-                {"", "", "Your username is invalid!"},
-                {"", "SuperSecretPassword!", "Your username is invalid!"},
-                {"tomsmith", "", "Your password is invalid!"}
-        };
-    }
-
-    @Test(dataProvider = "invalidCredentials")
-    public void testInvalidLogin(String usernameValue, String passwordValue, String expectedMessage) {
-        driver.get("https://the-internet.herokuapp.com/login");
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        WebElement usernameInput = wait.until(ExpectedConditions.elementToBeClickable(By.id("username")));
-        WebElement passwordInput = driver.findElement(By.id("password"));
-        WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
-
-        usernameInput.sendKeys(usernameValue);
-        passwordInput.sendKeys(passwordValue);
-        loginButton.click();
-
-        WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("flash")));
-        Assert.assertTrue(errorMessage.getText().contains(expectedMessage));
     }
 }
